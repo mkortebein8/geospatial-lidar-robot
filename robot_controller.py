01c91799b1ade6ea78fcdab39e1ddcf2cc96c0d0
Arlan Hegenbarth <hegen008@umn.edu>
2026-04-08 01:17:42 +0800

    add robot controls

---
+++ robot_controller.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+import time
+from Raspbot_Lib import Raspbot
+
+pygame.init()
+
+pygame.joystick.init()
+
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+joystick = joysticks[0]
+
+joystick.init()
+
+robot = Raspbot()
+
+while True:
+    try:
+        for event in pygame.event.get():
+            if event.type == pygame.JOYAXISMOTION:
+                forward_speed = -joystick.get_axis(1)
+                turn_speed = joystick.get_axis(0)
+
+                left_motor = forward_speed + turn_speed
+                right_motor = forward_speed - turn_speed
+                
+                robot.Ctrl_Car(0, 0, int(20*left_motor))
+                robot.Ctrl_Car(1, 0, int(20*left_motor))
+        
+                robot.Ctrl_Car(2, 0, int(20*right_motor))
+                robot.Ctrl_Car(3, 0, int(20*right_motor))
+
+                print("Left Motor: ", int(20*left_motor))
+                print("Right Motor: ", int(20*right_motor))
+                
+    except KeyboardInterrupt as e:
+        robot.Ctrl_Car(0, 0, 0)
+        robot.Ctrl_Car(1, 0, 0)
+        robot.Ctrl_Car(2, 0, 0)
+        robot.Ctrl_Car(3, 0, 0)
+        break
+        
